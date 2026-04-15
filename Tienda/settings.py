d7fa33279c4a12698e37660cbc26eab35506d221
Laura Sofia Aceros <lsacerosm@eafit.edu.co>
2026-04-15 10:43:51 -0500

    Tut04b P1: Configurar motor MySQL para AWS RDS

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'Tienda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME', 'tienda_db'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'secret_password'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
